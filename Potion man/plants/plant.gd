168de418a5aa7dc4d9f27734b7f8aa8e6014d8bb
Maksim Déry <112103704+mastercoder1999@users.noreply.github.com>
2026-02-10 13:15:47 -0500

    interactions

---
+++ Potion man/plants/plant.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_interaction_area_focused(interactor: Interactor) -> void:
+	pass # Replace with function body.
+
+
+func _on_interaction_area_interacted(interactor: Interactor) -> void:
+	pass # Replace with function body.
+
+
+func _on_interaction_area_unfocused(interactor: Interactor) -> void:
+	pass # Replace with function body.
