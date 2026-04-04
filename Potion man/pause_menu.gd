7784ae781d9edf408171173bfaeb7036316e826a
Maksim Déry <112103704+mastercoder1999@users.noreply.github.com>
2026-04-04 13:33:08 -0400

    Polished UI
    #7
    
    Going to start on the way the story and goal will be given to the player

--- Potion man/pause_menu.gd
+++ Potion man/pause_menu.gd
@@ -7,4 +7,10 @@ func _on_resume_pressed() -> void:
 
 
 func _on_quit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().quit()
+
+
+func _on_return_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
